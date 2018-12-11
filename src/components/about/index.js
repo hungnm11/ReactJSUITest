0fcff3bbba7de435e0042d1be97f117f1708bca9
Nguyen Minh Hung <hung.nm@tctav.com>
2018-12-11 14:11:03 +0700

    update UI

--- src/components/about/index.js
+++ src/components/about/index.js
@@ -3,15 +3,89 @@
 import React from 'react';
 
 const About = () => {
-	return (
-		<div className="container">
-			<div className="row">
-				<div className="col-md-12">
-					<h1>About Page</h1>
-				</div>
+	return <div className="container">
+		<div className="row">
+			<div className="col-md-12">
+				<h1 className="display-4">About Page</h1>
+				<p>
+            Lorem ipsum dolor sit amet, eu duis placerat scriptorem vel. Sit
+            atqui aliquip minimum in. Equidem debitis in vel, doming
+            appellantur sed ea, veri persequeris definitionem vis ea. Te mea
+            erant putant voluptatum.
+				</p>
+
+				<p>
+            Ea eos sensibus evertitur, verear fierent temporibus his ut. In
+            agam appareat scripserit vim, mei an feugait incorrupte
+            omittantur. Ius affert electram ea, duo veniam graeci eu. Ei vis
+            ipsum accumsan voluptaria, cu paulo concludaturque quo. Per in
+            probo ferri iisque, facer eleifend inimicus ea sit.
+				</p>
+
+				<p>
+            Repudiare similique persequeris quo at, ad ridens adipiscing has.
+            Eu mea menandri liberavisse consequuntur. Aeterno ullamcorper mel
+            ut. Ad nibh movet deserunt duo, tritani mandamus complectitur mei
+            id. Modo prompta moderatius ne quo, ea pro tota probo iusto.
+				</p>
+
+				<p>
+            Nemore similique ea duo, ius ut dolor sensibus, eu mel modo
+            antiopam. Usu tamquam singulis democritum ex. Duo hinc assentior
+            cu, no pri cetero temporibus. Sit et homero contentiones, admodum
+            docendi recteque duo cu. Ei eum dicat essent omittam.
+				</p>
+
+				<p>
+            Ea has possit splendide, quando adversarium in has. Quo vivendo
+            volumus salutandi eu. Audire aperiri sit te. Mucius impetus
+            labitur sit at, vel aperiri virtute vituperata ex, erant aliquam
+            ei his. Eos ut perfecto corrumpit incorrupte, mei ea dolor
+            incorrupte deterruisset.
+				</p>
+
+				<p>
+            Has te saepe quaeque. Nam te commune neglegentur, qui cu malis
+            iracundia omittantur. Ea vidit accusam has, sit unum constituam
+            cu, ex sit iracundia constituam. Pro no dolore populo torquatos,
+            case prompta philosophia vel ei, eum an suas debet. Qui corpora
+            legendos indoctum ad, vix clita ocurreret ut.
+				</p>
+
+				<p>
+            Nibh inani sea eu, id mei tempor scaevola. Qui te vero dignissim,
+            adipisci evertitur abhorreant ad nam, ius eu solet graeco. Mei eu
+            salutandi instructior, legimus gubergren disputando his ut, eam at
+            vivendum recteque voluptaria. Stet tota sea ut, ad eos liber
+            dicant. Eros persius offendit usu et, comprehensam vituperatoribus
+            et mea. Pri eu omnis quodsi argumentum, eum cu blandit voluptaria,
+            eruditi torquatos nam et. Usu elit summo corrumpit no.
+				</p>
+
+				<p>
+            Ut esse dignissim tincidunt quo. Erant omnium evertitur per te.
+            Eam illud officiis ne, vix nulla invidunt id. Cu sit dignissim
+            vituperata delicatissimi, quaerendum mediocritatem at per. Ut usu
+            magna dicit erroribus, sed et dicat altera.
+				</p>
+
+				<p>
+            Per an essent fabellas, sit at voluptua comprehensam. Pro eu dico
+            erant liberavisse. Admodum aliquando qui in, at civibus accumsan
+            platonem sed. Labores eloquentiam ut quo, audiam reprimique
+            accommodare eam ut.
+				</p>
+
+				<p>
+            In fugit atqui disputationi est, oblique suscipiantur et nec, in
+            ornatus inciderint mei. Pri inani sensibus eloquentiam cu, est
+            legere iuvaret postulant eu. Errem diceret feugiat at pro. Per
+            vocent adversarium ut, nibh principes consetetur at vix, nisl
+            fabellas ex eos.
+				</p>
 			</div>
 		</div>
-	);
+	</div>;
 };
 
 export default About;
