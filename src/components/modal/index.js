53d3ed367f2d53565fc3d2ab1f3912953e41e858
Nguyen Minh Hung <hung.nm@tctav.com>
2018-12-12 09:28:50 +0700

    update code

--- src/components/modal/index.js
+++ src/components/modal/index.js
@@ -1,26 +1,36 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 /* eslint-disable no-unused-vars */
-import React from 'react';
-import { Button, Modal, ModalHeader, ModalBody, ModalFooter, CardImg } from 'reactstrap';
+import React, { Component } from 'react';
+import { 
+	Button, 
+	Modal, 
+	ModalHeader, 
+	ModalBody, 
+	ModalFooter, 
+	CardImg,
+	Carousel,
+  CarouselItem,
+  CarouselControl,
+  CarouselIndicators,
+  CarouselCaption
+} from 'reactstrap';
 
-const ModalUI = ({
-	isOpen,
-	toggle,
-	detail
-}) => {
-	const { title, image, desc } = detail;
-	return (
-		<Modal isOpen={isOpen} toggle={toggle.bind(this, '', '', '')} >
-			<ModalHeader toggle={toggle}>{title}</ModalHeader>
-			<ModalBody>
-				<CardImg src={image} />
-				{desc}
-			</ModalBody>
-			<ModalFooter>
-				<Button color="secondary" onClick={toggle.bind(this, '', '', '')}>Close</Button>
-			</ModalFooter>
-		</Modal>
-	);
+class ModalUI extends Component {
+	render() {
+		const { isOpen, toggle, detail } = this.props;
+		return (
+			<Modal isOpen={isOpen} toggle={toggle.bind(this, '', '', '')} >
+				<ModalHeader toggle={toggle}>{detail.title}</ModalHeader>
+				<ModalBody>
+					<CardImg src={detail.image} />
+					{detail.desc}
+				</ModalBody>
+				<ModalFooter>
+					<Button color="secondary" onClick={toggle.bind(this, '', '', '')}>Close</Button>
+				</ModalFooter>
+			</Modal>
+		);
+	}
 };
 
 export default ModalUI;
