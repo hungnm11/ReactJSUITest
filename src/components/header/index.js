71d3d6f331d4365c6650e84953a56123c93a1e40
Nguyen Minh Hung <hung.nm@tctav.com>
2018-12-12 09:05:50 +0700

    update UI

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,40 +1,55 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 /* eslint-disable no-unused-vars */
-import React from 'react';
+import React, { Component } from 'react';
+import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
-const Header  = () => {
-	return (
-		<div>
-			<nav className="navbar navbar-expand-lg navbar-light bg-light">
-				<a className="navbar-brand" href="#">Demo</a>
-				<button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-					<span className="navbar-toggler-icon"></span>
-				</button> 
+class Header extends Component {
 
-				<div className="collapse navbar-collapse" id="navbarSupportedContent">
-					<ul className="navbar-nav">
-						<li className="nav-item active">
-							<Link className="nav-link" to="/">HOME<span className="sr-only">(current)</span></Link>
-						</li>
-						<li className="nav-item">
-							<Link className="nav-link" to="/about">ABOUT</Link>
-						</li>
-						<li className="nav-item">
-							<Link className="nav-link disabled" to="/contact">CONTACT</Link>
-						</li>
-					</ul>
-				</div>
-			</nav>
+	constructor(props) {
+    super(props);
 
-			<div className="jumbotron jumbotron-fluid">
-				<div className="container">
-					<h1 className="display-4">Fluid jumbotron</h1>
-					<p className="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+
+	render() {
+		return (
+			<div>
+				<Navbar color="light" light expand="md">
+          <NavbarBrand href="/">DEMO</NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <NavLink href="/">HOME</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="/about">ABOUT</NavLink>
+              </NavItem>
+							<NavItem>
+                <NavLink href="/contact">CONTACT</NavLink>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Navbar>
+	
+				<div className="jumbotron jumbotron-fluid">
+					<div className="container">
+						<h1 className="display-4">Fluid jumbotron</h1>
+						<p className="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+					</div>
 				</div>
 			</div>
-		</div>
-	);
+		);
+	}
 };
 
 export default Header;
